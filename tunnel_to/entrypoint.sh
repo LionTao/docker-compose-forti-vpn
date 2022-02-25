8fe13aa0440a35b704b8a50ebb745cf02d291bcc
LionTao <taojiachun980831@163.com>
2022-02-25 18:45:06 +0800

    breaking change: tunnel_to using default gw

--- tunnel_to/entrypoint.sh
+++ tunnel_to/entrypoint.sh
@@ -2,12 +2,8 @@
 
 echo "Setting routes..."
 
-# Add your route for your vpn here
-# remember to bypass your own network at home
-ip route add 192.168.131.0/24 via 172.22.0.2
-ip route add 192.168.126.0/24 via 172.22.0.2
-ip route add 10.0.0.0/8 via 172.22.0.2
-ip route add 42.244.0.0/16 via 172.22.0.2
+ip route delete default
+ip route add default via 172.22.0.2
 
 echo "Done"
 
