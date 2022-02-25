2545f4580ffe84a16d6af740b7c1350a4dd87f69
LionTao <taojiachun980831@163.com>
2022-02-25 14:34:04 +0800

    fix routes

--- forti/ppp0-up.sh
+++ forti/ppp0-up.sh
@@ -3,7 +3,10 @@
 if [ "$PPP_IFACE" != "ppp0" ]; then exit 0; fi
 echo 'Setting routes...'
 
-ip route add 192.168.0.0/16 dev ppp0
+# Add your route for your vpn here
+# remember to bypass your own network at home
+ip route add 192.168.131.0/24 dev ppp0
+ip route add 192.168.126.0/24 dev ppp0
 ip route add 10.0.0.0/8 dev ppp0
 ip route add 42.244.0.0/16 dev ppp0
 
--- tunnel_to/entrypoint.sh
+++ tunnel_to/entrypoint.sh
@@ -2,7 +2,10 @@
 
 echo "Setting routes..."
 
-ip route add 192.168.0.0/16 via 172.22.0.2
+# Add your route for your vpn here
+# remember to bypass your own network at home
+ip route add 192.168.131.0/24 via 172.22.0.2
+ip route add 192.168.126.0/24 via 172.22.0.2
 ip route add 10.0.0.0/8 via 172.22.0.2
 ip route add 42.244.0.0/16 via 172.22.0.2
 
